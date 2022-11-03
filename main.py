617edca6ff52f719be21ab12ce5a3781874bc922
sergeyzaitsev <kitamhol@gmail.com>
2022-11-03 11:11:25 +0300

    в 4 строчке укоротил фразу

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 todo_list = []
 
 while True:
-    print('Put down your', str(len(todo_list) + 1), 'task you gonna do bro, I`ll try to keep it OR enter nothing to quit')
+    print('Put down your', str(len(todo_list) + 1), 'task you gonna do bro OR enter nothing to quit')
     new_task = input()
     if new_task == '':
         break
     todo_list = todo_list + [new_task]
 
 print('you`ve got', str(len(todo_list)), 'tasks in yor ToDo list. Here it is:', todo_list)
+
