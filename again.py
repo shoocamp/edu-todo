1061807e2e7dc3c0e1d8f86494d014059f93bec3
sergeyzaitsev <kitamhol@gmail.com>
2023-01-08 11:48:19 +0300

    all function are methods now. and I made them more universal. i`m gonna put some filters kinda valueerror exceptions further

--- again.py
+++ again.py
@@ -13,50 +13,38 @@ class Base:
         self.storage = []
 
 
+    def add_task(self, descriprion):
+        task = Task(descriprion)
+        self.storage.append(task)
+        print(f'task "{descriprion}" added')
+
+
+    def set_status(self, task_id, new_status):
+        task = self.storage[task_id]
+        task.status = new_status
+        print(f'status of "{task.description}" replaced by {new_status}')
+
+    def show_specific_list(self, specific_status):
+        for task in self.storage:
+            if task.status == specific_status:
+                print(f"{specific_status} tasks here: {task.description} : {task.status}")
+
+
 main_base = Base('Main base')
 
-
-def add_task(descriprion):
-    task = Task(descriprion)
-    main_base.storage.append(task)
-    print(f'task "{descriprion}" added')
-
-
-def set_status_done(task_id):
-    task = main_base.storage[task_id]
-    task.status = 'done'
-    print(f'status of "{task.description}" replaced DONE')
-
-
-def show_new():
-    for t in main_base.storage:
-        if t.status == "done":
-            continue
-        else:
-            print(f"{t.description} - {t.status}")
-
-
-def show_archive():
-    for t in main_base.storage:
-        if t.status == "new":
-            continue
-        else:
-            print(f"{t.description} - {t.status}")
-
-
-add_task('lup')
-add_task('pupa')
-add_task('zukko')
-add_task('pipika')
-add_task('hophopa')
-add_task('zhuzhuzhu')
+main_base.add_task('lup')
+main_base.add_task('pupa')
+main_base.add_task('zukko')
+main_base.add_task('pipika')
+main_base.add_task('hophopa')
+main_base.add_task('zhuzhuzhu')
 
 print(main_base.storage)  # checking whole the list
 
-set_status_done(2)        # test editing task status
-set_status_done(4)
+main_base.set_status(2, "done")        # test editing task status
+main_base.set_status(4, "done")
 
 print(main_base.storage)  # checking whole the list whith changes
 
-show_new()                # checking how functions work
-show_archive()
+main_base.show_specific_list("done")                # checking how functions work
+# show_archive()
