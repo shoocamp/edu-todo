8952e93f04224638614b2ef1938dda6b19ac1756
sergeyzaitsev <kitamhol@gmail.com>
2023-01-17 18:42:02 +0300

    я не помню менял ли я что-то. просто сейчас на всякий обновлю и потом форкну

--- ToDo_CU.py
+++ ToDo_CU.py
@@ -1,10 +1,15 @@
+from _datetime import datetime
+
+
 class Task:
-    def __init__(self, description, status='NEW'):
+    def __init__(self, description, status='NEW', start_date=datetime.today(), due_date='NO SET'):
         self.description = description
         self.status = status
+        self.start_date = start_date
+        self.due_date = due_date
 
     def __repr__(self):
-        return f"{self.description} - {self.status}"
+        return f"{self.description}/{self.status}/{self.start_date}/{self.due_date}"
 
 
 class TaskStorage:
@@ -12,10 +17,9 @@ class TaskStorage:
         self.description = description
         self.storage = []
 
-    def add_task(self, descriprion):
-        task = Task(descriprion)
+    def add_task(self, task):
         self.storage.append(task)
-        print(f'task "{descriprion}" added')
+        print(f'task "{task.description}" added')
 
     def edit_description(self, task_id, new_description):
         task = self.storage[task_id]
@@ -40,12 +44,12 @@ main_storage = TaskStorage('Main base')
 
 if __name__ == "__main__":
 
-    main_storage.add_task('lup')
-    main_storage.add_task('pupa')
-    main_storage.add_task('zukko')
-    main_storage.add_task('pipika')
-    main_storage.add_task('hophopa')
-    main_storage.add_task('zhuzhuzhu')
+    # main_storage.add_task('lup')
+    # main_storage.add_task('pupa')
+    # main_storage.add_task('zukko')
+    # main_storage.add_task('pipika')
+    # main_storage.add_task('hophopa')
+    # main_storage.add_task('zhuzhuzhu')
 
     print(main_storage.storage)  # checking whole the list
 
--- UI.py
+++ UI.py
@@ -1,14 +1,16 @@
-import again
+import ToDo_CU
 
 
-def print_list_with_indexes():  # printing of list with indexes
-    for (i, item) in enumerate(again.main_storage.storage, start=1):
+def print_list_with_indexes():
+    """printing of list with indexes"""
+    for (i, item) in enumerate(ToDo_CU.main_storage.storage, start=1):
         print(i, item)
 
 
-def main_menu():  # printing of main menu & UI processing
+def main_menu():
+    """printing of main menu & UI processing"""
     print('\n 1- add new task \n 2- edit description \n 3- edit status '
-          '\n 4- show all tasks \n 5- show archive \n 6- show new tasks \n')
+          f'\n 4- show all tasks ({len(ToDo_CU.main_storage.storage)}) \n 5- show archive \n 6- show new tasks \n')
     try:
         ui = int(input())
         return ui
@@ -16,7 +18,8 @@ def main_menu():  # printing of main menu & UI processing
         print('You have to put only one digit from the list below.')
 
 
-def menu_for_choosing_tasks_to_change():  # printing of task list to choosing task & UI processing
+def menu_for_choosing_tasks_to_change():
+    """printing of task list to choosing task & UI processing"""
     print('Choose task')
     print_list_with_indexes()
     try:
@@ -26,19 +29,25 @@ def menu_for_choosing_tasks_to_change():  # printing of task list to choosing ta
         print('You have to put only one digit from the list.')
 
 
+def checking_if_list_empty():
+    if len(ToDo_CU.main_storage.storage) == 0:
+        print("List is empty yet. Put some tasks to start.")
+
+
 while True:
     ui_main_menu = main_menu()
 
     if ui_main_menu == 1:
         task_description = input('Task description?\n')
-        again.main_storage.add_task(task_description)
-        print(again.main_storage.storage)
+        task = ToDo_CU.Task(task_description)
+        ToDo_CU.main_storage.add_task(task)
+        print(ToDo_CU.main_storage.storage)
 
     elif ui_main_menu == 2:
         task_id = menu_for_choosing_tasks_to_change()
         new_description = input('New description:\n')
-        again.main_storage.edit_description(task_id-1, new_description)
-        print(f'Result is: {again.main_storage.storage[task_id-1]}\nAnd all list is:{again.main_storage.storage}')
+        ToDo_CU.main_storage.edit_description(task_id-1, new_description)
+        print(f'Result is: {ToDo_CU.main_storage.storage[task_id-1]}\nAnd all list is:{ToDo_CU.main_storage.storage}')
 
     elif ui_main_menu == 3:
         print('Choose task')
@@ -46,17 +55,20 @@ while True:
         task_id = int(input())
         new_status = int(input("You can choose 1 - DONE or 2 - NEW status \n"))
         if new_status == 1:
-            again.main_storage.set_status(task_id - 1, 'DONE')
+            ToDo_CU.main_storage.set_status(task_id - 1, 'DONE')
             print_list_with_indexes()
         else:
-            again.main_storage.set_status(task_id - 1, 'NEW')
+            ToDo_CU.main_storage.set_status(task_id - 1, 'NEW')
             print_list_with_indexes()
 
     elif ui_main_menu == 4:
-        print(again.main_storage.storage)
+        checking_if_list_empty()
+        print(ToDo_CU.main_storage.storage)
 
     elif ui_main_menu == 5:
-        again.main_storage.show_specific_list('done')
+        checking_if_list_empty()
+        ToDo_CU.main_storage.show_specific_list('done')
 
     elif ui_main_menu == 6:
-        again.main_storage.show_specific_list('new')
+        checking_if_list_empty()
+        ToDo_CU.main_storage.show_specific_list('new')
